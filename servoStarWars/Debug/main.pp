275601213208a051b76ea960fbe22d464a10a30d
valerak2000 <valera_k2000@inbox.ru>
2016-07-26 12:31:28 +0300

    init

---
+++ servoStarWars/Debug/main.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: E:/work/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: E:/work/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: E:/work/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.7/include/intrinsics.h
+main.obj: E:/work/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.7/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+E:/work/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+E:/work/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+E:/work/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.7/include/intrinsics.h: 
+E:/work/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.7/include/intrinsics_legacy_undefs.h: 
